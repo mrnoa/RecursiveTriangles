0280c2b8e5e11d198c379e44843468c5618468cb
mrnoa <roman.krajewski@gmx.de>
2016-06-20 12:44:21 +0200

    final final commit

--- Triangles.java
+++ Triangles.java
@@ -3,14 +3,11 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Polygon;
-import java.awt.Rectangle;
-import java.awt.event.ComponentEvent;
 
 import javax.swing.JFrame;
 
 public class Triangles extends Canvas {
 	private JFrame pane;
-	private Canvas canvas;
 	private int height = 1000;
 	private Polygon polygon;
 	private int numberOfSteps = 6;
@@ -31,27 +28,25 @@ public class Triangles extends Canvas {
 	}
 
 	private void drawTriangles(int n, int[] origXPoints, int[] origYPoints, Graphics g) {
-			int redValue =Math.min(255,n*255/numberOfSteps);
-			g.setColor(new Color(255-redValue,255,0));
-			int[] xPoints = { (origXPoints[0] + origXPoints[1]) / 2, (origXPoints[1] + origXPoints[2]) / 2,
-					(origXPoints[2] + origXPoints[0]) / 2 };
-			int[] yPoints = { (origYPoints[0] + origYPoints[1]) / 2, (origYPoints[1] + origYPoints[2]) / 2,
-					(origYPoints[2] + origYPoints[0]) / 2 };
-			Polygon polygon = new Polygon(xPoints, yPoints, 3);
-			
-			
-			g.fillPolygon(polygon);
-			g.setColor(Color.BLACK);
-			g.drawPolygon(polygon);
-			if (numberOfSteps -1 > n) {
-			drawTriangles(n+1,new int[] { xPoints[0], xPoints[2], origXPoints[0] },
+		int redValue = Math.min(255, n * 255 / numberOfSteps);
+		g.setColor(new Color(255 - redValue, 255, 0));
+		int[] xPoints = { (origXPoints[0] + origXPoints[1]) / 2, (origXPoints[1] + origXPoints[2]) / 2,
+				(origXPoints[2] + origXPoints[0]) / 2 };
+		int[] yPoints = { (origYPoints[0] + origYPoints[1]) / 2, (origYPoints[1] + origYPoints[2]) / 2,
+				(origYPoints[2] + origYPoints[0]) / 2 };
+		Polygon polygon = new Polygon(xPoints, yPoints, 3);
+
+		g.fillPolygon(polygon);
+		g.setColor(Color.BLACK);
+		g.drawPolygon(polygon);
+		if (numberOfSteps - 1 > n) {
+			drawTriangles(n + 1, new int[] { xPoints[0], xPoints[2], origXPoints[0] },
 					new int[] { yPoints[0], yPoints[2], origYPoints[0] }, g);
-			drawTriangles(n+1,new int[] { xPoints[1], xPoints[0], origXPoints[1] },
+			drawTriangles(n + 1, new int[] { xPoints[1], xPoints[0], origXPoints[1] },
 					new int[] { yPoints[1], yPoints[0], origYPoints[1] }, g);
-			drawTriangles(n+1,new int[] { xPoints[2], xPoints[1], origXPoints[2] },
+			drawTriangles(n + 1, new int[] { xPoints[2], xPoints[1], origXPoints[2] },
 					new int[] { yPoints[2], yPoints[1], origYPoints[2] }, g);
 
-			
 		}
 	}
 
@@ -63,13 +58,9 @@ public class Triangles extends Canvas {
 		int[] xPoints = { height, 0, height - x };
 		int[] yPoints = { height, height - x, 0 };
 		polygon = new Polygon(xPoints, yPoints, 3);
-		// int[] middleXPoints = {(xPoints[0]+xPoints[1])/2,(xPoints[1]+xPoints[2])/2,(xPoints[2]+xPoints[0])/2 };
-		// int[] middleYPoints = {(yPoints[0]+yPoints[1])/2,(yPoints[1]+yPoints[2])/2,(yPoints[2]+yPoints[0])/2 };
-		// Polygon middleTriangle = new Polygon(middleXPoints, middleYPoints, 3);
 		g.setColor(Color.black);
 		g.drawPolygon(polygon);
-		drawTriangles(0,xPoints, yPoints, g);
-		// g.drawPolygon(middleTriangle);
+		drawTriangles(0, xPoints, yPoints, g);
 	}
 
 }
